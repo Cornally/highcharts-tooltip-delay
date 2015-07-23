5338902aabf5a7463ed0dd73ccb4c9e78cc1f2d6
Chris Connelly <22bpower@gmail.com>
2015-07-23 11:20:14 -0400

    Support has been added to opt out of a default delay (no delay at all) by setting the "delayForDisplay" option to false.  Documentation updated.

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Highcharts tooltip delay
 
 Add delay before displaying Highcharts Tooltip
 
-Home page with examples 
+Home page with examples
 http://rudovjan.github.io/highcharts-tooltip-delay/
 
 Quick demo - http://jsfiddle.net/rudovsky/pu3x5/
+
+Set the highchart tooltip "delayToDisplay" option to a numerical value in milliseconds to delay showing the tooltip -- e.g. `delayToDisplay: 300`.  A default value of 1000 milliseconds is applied when the user does not specify a value.  You may opt out of the delay entirely by setting `delayToDisplay: false`.
--- tooltip-delay.js
+++ tooltip-delay.js
@@ -4,24 +4,27 @@
 
     H.wrap(H.Tooltip.prototype, 'refresh', function (proceed) {
 
-
         var point = arguments[1];
         var chart = this.chart;
         var tooltip = this;
         var refreshArguments = arguments;
-        var delayForDisplay = chart.options.tooltip.delayForDisplay : chart.options.tooltip.delayForDisplay ? 1000;
+        var delayForDisplay = chart.options.tooltip.delayForDisplay !== undefined ? chart.options.tooltip.delayForDisplay : 1000;
 
         if (timerId) {
             clearTimeout(timerId);
         }
 
-        timerId = window.setTimeout(function () {
+        if (delayForDisplay !== false) {
+            timerId = window.setTimeout(function () {
 
-            if (point === chart.hoverPoint || $.inArray(chart.hoverPoint, point) > -1) {
-                proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
-            }
+                if (point === chart.hoverPoint || $.inArray(chart.hoverPoint, point) > -1) {
+                    proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
+                }
 
-        }, delayForDisplay);
+            }, delayForDisplay);
+        } else {
+            proceed.apply(tooltip, Array.prototype.slice.call(refreshArguments, 1));
+        }
  
     });
 
